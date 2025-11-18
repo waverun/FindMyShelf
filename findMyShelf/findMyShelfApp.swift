d5c72de2486d94b9b7f190081a42377257d27b98
Shay Moreno <moa129@gmail.com>
2025-11-18 11:44:27 +0200

    Initial Commit

---
+++ findMyShelf/findMyShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  findMyShelfApp.swift
+//  findMyShelf
+//
+//  Created by shay moreno on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct findMyShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
