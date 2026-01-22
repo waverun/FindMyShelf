b3c08172f0965c18fd657ead0e191de697cbf003
Shay Moreno <moa129@gmail.com>
2026-01-23 00:34:58 +0200

    add firebase

--- findMyShelf/findMyShelfApp.swift
+++ findMyShelf/findMyShelfApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct findMyShelfApp: App {
+
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
