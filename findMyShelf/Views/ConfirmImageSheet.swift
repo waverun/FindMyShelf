f00af346e9c8628466b842fe55819805471cf6d8
Shay Moreno <moa129@gmail.com>
2026-05-13 14:59:48 +0300

    אפשרות להגדלת תמונה

--- findMyShelf/Views/ConfirmImageSheet.swift
+++ findMyShelf/Views/ConfirmImageSheet.swift
@@ -17,10 +17,7 @@ struct ConfirmImageSheet: View {
                         .aspectRatio(3/4, contentMode: .fit)
 
                     if let image {
-                        Image(uiImage: image)
-                            .resizable()
-                            .scaledToFit()
-                            .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                        ZoomableImageView(image: image)
                             .transition(.opacity)
                     } else {
                         ProgressView("Loading image…")
@@ -63,5 +60,85 @@ struct ConfirmImageSheet: View {
                 }
             }
         }
+    }
+}
+
+private struct ZoomableImageView: View {
+    let image: UIImage
+
+    @State private var baseScale: CGFloat = 1.0
+    @GestureState private var pinchScale: CGFloat = 1.0
+
+    @State private var baseOffset: CGSize = .zero
+    @GestureState private var dragOffset: CGSize = .zero
+
+    private let minScale: CGFloat = 1.0
+    private let maxScale: CGFloat = 4.0
+
+    private var effectiveScale: CGFloat {
+        min(max(baseScale * pinchScale, minScale), maxScale)
+    }
+
+    private var effectiveOffset: CGSize {
+        CGSize(
+            width: baseOffset.width + dragOffset.width,
+            height: baseOffset.height + dragOffset.height
+        )
+    }
+
+    var body: some View {
+        Image(uiImage: image)
+            .resizable()
+            .scaledToFit()
+            .scaleEffect(effectiveScale)
+            .offset(effectiveOffset)
+            .contentShape(Rectangle())
+            .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+            .gesture(combinedGesture)
+            .onTapGesture(count: 2) {
+                withAnimation(.easeInOut(duration: 0.2)) {
+                    baseScale = 1.0
+                    baseOffset = .zero
+                }
+            }
+    }
+
+    private var combinedGesture: some Gesture {
+        SimultaneousGesture(magnificationGesture, dragGesture)
+    }
+
+    private var magnificationGesture: some Gesture {
+        MagnificationGesture()
+            .updating($pinchScale) { value, state, _ in
+                state = value
+            }
+            .onEnded { value in
+                baseScale = min(max(baseScale * value, minScale), maxScale)
+                if baseScale <= 1.01 {
+                    baseScale = 1.0
+                    baseOffset = .zero
+                }
+            }
+    }
+
+    private var dragGesture: some Gesture {
+        DragGesture()
+            .updating($dragOffset) { value, state, _ in
+                guard effectiveScale > 1.0 else {
+                    state = .zero
+                    return
+                }
+                state = value.translation
+            }
+            .onEnded { value in
+                guard effectiveScale > 1.0 else {
+                    baseOffset = .zero
+                    return
+                }
+                baseOffset = CGSize(
+                    width: baseOffset.width + value.translation.width,
+                    height: baseOffset.height + value.translation.height
+                )
+            }
     }
 }
